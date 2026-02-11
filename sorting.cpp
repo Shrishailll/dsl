4e75cbc3b9621707f9c6db78bf34efd78b0c28df
Shrishailll <shrishailjamadar901@gmail.com>
2026-02-11 10:07:14 +0530

    Refactor sorting menu and bubble sort cases

--- sorting.cpp
+++ sorting.cpp
@@ -4,20 +4,19 @@ using namespace std;
 int main() {
     int n, choice, temp;
 
-    // Get array size from user
+    
     cout << "Enter the size of the array: ";
     cin >> n;
 
-    int arr[n]; // Declare array of given size
-
-    // Get array elements from user
+    int arr[n]; 
+    
     cout << "Enter the elements:" << endl;
     for (int i = 0; i < n; i++) {
         cout << "Element " << i + 1 << ": ";
         cin >> arr[i];
     }
 
-    // Menu for sorting options
+   
     do {
         cout << "\n--- Sorting Menu ---" << endl;
         cout << "1. Sort in Ascending Order" << endl;
@@ -28,11 +27,11 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-            case 1: // Ascending Order using Bubble Sort
+            case 1: 
                 for (int i = 0; i < n; i++) {
                     for (int j = i + 1; j < n; j++) {
                         if (arr[i] > arr[j]) {
-                            // Swap elements
+                            
                             temp = arr[i];
                             arr[i] = arr[j];
                             arr[j] = temp;
@@ -42,11 +41,11 @@ int main() {
                 cout << "\nArray sorted in ascending order." << endl;
                 break;
 
-            case 2: // Descending Order using Bubble Sort
+            case 2:
                 for (int i = 0; i < n; i++) {
                     for (int j = i + 1; j < n; j++) {
                         if (arr[i] < arr[j]) {
-                            // Swap elements
+                            
                             temp = arr[i];
                             arr[i] = arr[j];
                             arr[j] = temp;
@@ -56,7 +55,7 @@ int main() {
                 cout << "\nArray sorted in descending order." << endl;
                 break;
 
-            case 3: // Display Array
+            case 3: 
                 cout << "\nCurrent array elements: ";
                 for (int i = 0; i < n; i++) {
                     cout << arr[i] << " ";
@@ -64,7 +63,7 @@ int main() {
                 cout << endl;
                 break;
 
-            case 4: // Exit
+            case 4: 
                 cout << "Exiting program. Goodbye!" << endl;
                 break;
 
